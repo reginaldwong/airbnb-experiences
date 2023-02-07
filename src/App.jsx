bed8d22986ea5f07d6840f718f84a683693ea865
Reginald Wong <reginald_wong@outlook.com>
2023-02-07 15:10:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Card />
+    </div>
+  )
+}
+
+export default App;
