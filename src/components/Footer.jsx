2f3412de3b461e98775be6cc5112f8116e9a28f4
Reginald Wong <reginald_wong@outlook.com>
2023-02-08 16:24:22 -0500

    add anchor tag to footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const Footer = () => {
     return (
         <footer>
-            <p>Project by Reginald Wong</p>
+            <p className="footer--name">Project by <a href="https://reginaldwong.com" target="_blank">Reginald Wong</a></p>
             <ul className="footer--links">
-                <li><a href="https://github.com/reginaldwong" target="_blank"><img src="src/assets/github.svg" alt="" /></a></li>
+                <li><a href="https://github.com/reginaldwong/airbnb-experiences" target="_blank"><img src="src/assets/github.svg" alt="" /></a></li>
                 <li><a href="https://linkedin.com/in/reginaldwong" target="_blank"><img src="src/assets/linkedin.svg" alt="" /></a></li>
                 <li><a href="https://reginaldwong.com" target="_blank"><img src="src/assets/user.svg" alt="" /></a></li>
             </ul>
