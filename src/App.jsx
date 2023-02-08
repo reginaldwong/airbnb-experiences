751cc97167edb1b6bf0d5d0ec40226efd8565629
Reginald Wong <reginald_wong@outlook.com>
2023-02-07 20:20:47 -0500

    style card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        price={item.price}
+        image={item.coverImg}
+        rating={item.stats.rating}
+        review={item.stats.reviewCount}
+        location={item.location}
+        title={item.title} 
+      />
+    )
+  })
+
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section>
+        {cards}
+      </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,20 @@
 import React from "react"
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <h1>Card</h1>
+        <div className="card">
+            <img src={`src/assets/${props.image}`} />
+            <div className="card--stats">
+                <img src="src/assets/star.png" alt="star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.review})</span> • <span className="gray">{props.location}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
+        </div>
     )
+        
+       
 }
 
 export default Card;
